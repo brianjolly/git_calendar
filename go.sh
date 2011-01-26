b96278efcea48e41c75b9b91508c734f556cda34
Brian Jolly <brian@creativelift.net>
2011-01-26 15:32:16 -0800

    Simplified go.sh and made config a little clearer

--- go.sh
+++ go.sh
@@ -2,6 +2,10 @@
 
 # update arguments with your name or email that git would recognize
 # and enter a date range
-# ruby git_calendar.rb <your name> <your projects folder path> <start date> <end date> > out.csv
-ruby git_calendar.rb Brian '/Users/brian/projects' 'Jan 10' 'Today' > out.csv
-ruby csv_to_json.rb out.csv > httpdocs/js/out.json
+
+name='Brian'
+projects_folder="/Users/brian/projects"
+start_date="Jan 20"
+end_date="Today"
+
+ruby git_calendar.rb "$name" "$projects_folder" "$start_date" "$end_date" > ruby csv_to_json.rb > httpdocs/js/out.json
